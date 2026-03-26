6d72baa00c95f8d89c0b92dca57b53d73352b279
Alastor-XIII <alastor0104@gmail.com>
2026-03-26 10:35:43 +0700

    Update Milestones.py

--- pages/Milestones.py
+++ pages/Milestones.py
@@ -1,25 +1,15 @@
 import streamlit as st
+from utils.load_data import load_milestones
 
 if "project" not in st.session_state:
     st.warning("Please select project from Home")
     st.stop()
 
-project = st.session_state.get("project")
+project = st.session_state["project"]
 
-st.title(f"🏁 Milestones - {project}")
+st.title(f"Milestones - {project}")
 
 df = load_milestones()
 df = df[df["Project"] == project]
 
-st.dataframe(df, use_container_width=True)
-
-completed = len(df[df["Status"] == "Done"])
-total = len(df)
-
-if total > 0:
-    percent = (completed / total) * 100
-else:
-    percent = 0
-
-st.progress(int(percent))
-st.write(f"{percent:.1f}% Complete")
+st.dataframe(df)
