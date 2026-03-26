217db23373239b15b08d11e8ecf4869b3a71f1e4
Alastor-XIII <alastor0104@gmail.com>
2026-03-26 10:03:34 +0700

    Update Timeline.py

--- pages/Timeline.py
+++ pages/Timeline.py
@@ -1,26 +1,7 @@
 import streamlit as st
-import pandas as pd
-import plotly.express as px
-from datetime import datetime
 
-st.title("📅 Project Timeline")
+if "project" not in st.session_state:
+    st.warning("Please select project from Projects Overview")
+    st.stop()
 
-project = st.session_state.get("project")
-
-df = pd.read_excel("data/activities.xlsx")
-df = df[df["ProjectID"] == project]
-
-fig = px.timeline(
-    df,
-    x_start="Start",
-    x_end="Finish",
-    y="Activity",
-    color="Progress"
-)
-
-fig.update_yaxes(autorange="reversed")
-
-today = datetime.today().strftime('%Y-%m-%d')
-fig.add_vline(x=today, line_dash="dash", line_color="red")
-
-st.plotly_chart(fig, use_container_width=True)
+project = st.session_state["project"]
