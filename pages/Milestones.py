25d437456ca1f570f40c875fea110e2f4eecdc43
Alastor-XIII <alastor0104@gmail.com>
2026-03-26 10:06:27 +0700

    Update Milestones.py

--- pages/Milestones.py
+++ pages/Milestones.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 if "project" not in st.session_state:
-    st.warning("Please select project from Projects Overview")
+    st.warning("Please select project first")
     st.stop()
 
 project = st.session_state["project"]
