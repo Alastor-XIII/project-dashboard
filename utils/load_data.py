f5936fc04d07d8875c2274a4aaaf883e5f966d72
Alastor-XIII <alastor0104@gmail.com>
2026-03-26 10:17:40 +0700

    Update load_data.py

--- utils/load_data.py
+++ utils/load_data.py
@@ -1,17 +1,13 @@
 import pandas as pd
-from pathlib import Path
-
-BASE_DIR = Path(__file__).resolve().parent.parent
-DATA_DIR = BASE_DIR / "data"
 
 def load_projects():
-    return pd.read_excel(DATA_DIR / "projects.xlsx")
+    return pd.read_excel("data/projects.xlsx")
 
 def load_activities():
-    return pd.read_excel(DATA_DIR / "activities.xlsx")
+    return pd.read_excel("data/activities.xlsx")
 
 def load_milestones():
-    return pd.read_excel(DATA_DIR / "milestones.xlsx")
+    return pd.read_excel("data/milestones.xlsx")
 
 def load_resources():
-    return pd.read_excel(DATA_DIR / "resources.xlsx")
+    return pd.read_excel("data/resources.xlsx")
