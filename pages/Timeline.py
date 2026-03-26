b0dd08fbadf335413cab527fd2b65ed1a4c6928f
Alastor-XIII <alastor0104@gmail.com>
2026-03-26 10:19:59 +0700

    Update Timeline.py

--- pages/Timeline.py
+++ pages/Timeline.py
@@ -1,7 +1,22 @@
 import streamlit as st
+import plotly.express as px
+from utils.load_data import load_activities
 
-if "project" not in st.session_state:
-    st.warning("Please select project from Projects Overview")
-    st.stop()
+st.title("📅 Project Timeline")
 
-project = st.session_state["project"]
+df = load_activities()
+
+project = st.selectbox("Select Project", df["Project"].unique())
+df = df[df["Project"] == project]
+
+fig = px.timeline(
+    df,
+    x_start="Start",
+    x_end="Finish",
+    y="Activity",
+    color="Section"
+)
+
+fig.update_yaxes(autorange="reversed")
+
+st.plotly_chart(fig, use_container_width=True)
